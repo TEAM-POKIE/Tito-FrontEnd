74755a7466f39a427730cae29535b40fe7d44a46
oojoo <hosje7324@gmail.com>
2024-07-14 09:58:02 +0900

    [Fix] 처음 시작할 때 초기화면 route 지정

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //ProviderScope는 Riverpod 패키지에서 제공하는 위젯으로, 애플리케이션 전체에서 프로바이더 사용가능하도록 함
     return ProviderScope(
       child: MaterialApp.router(
         routerConfig: router,
         title: 'Tito',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.deepPurple,
+          //애플리케이션의 대표 테마 색상 -> const Color(0xFF8E48F8)
         ),
       ),
     );
