87f64b141638d59e2aa3cbbb624fa2fc648eeac7
oojoo <hosje7324@gmail.com>
2024-07-24 12:27:07 +0900

    [Feat] jieun

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 844), // 피그마에 디자인 된 앱 프레임 사이즈로 맞춰서 적용한 것
+      designSize: const Size(390, 844), // 피그마에 디자인 된 앱 프레임 사이즈로 맞춰서 적용한 것
       minTextAdapt: true,   // 텍스트 크기를 자동으로 조정하여 화면에 맞추는 기능을 활성화
       splitScreenMode: true,   // 분할 화면 모드를 활성화
       builder: (context, child) => ProviderScope(
