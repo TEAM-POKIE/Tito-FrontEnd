3b03577c0ba9d009f965a3767b9405280f52c0ad
oojoo <hosje7324@gmail.com>
2024-07-24 19:24:00 +0900

    [Feat] 반응형 UI 시작

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tito_app/src/view/splash_screen.dart';
 import 'core/routes/routes.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tito_app/core/constants/style.dart';
 
 void main() async {
   SplashScreen();
@@ -18,14 +19,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(390, 844), // 피그마에 디자인 된 앱 프레임 사이즈로 맞춰서 적용한 것
-      minTextAdapt: true,   // 텍스트 크기를 자동으로 조정하여 화면에 맞추는 기능을 활성화
-      splitScreenMode: true,   // 분할 화면 모드를 활성화
+      minTextAdapt: true, // 텍스트 크기를 자동으로 조정하여 화면에 맞추는 기능을 활성화
+      splitScreenMode: true, // 분할 화면 모드를 활성화
       builder: (context, child) => ProviderScope(
         child: MaterialApp.router(
           routerConfig: router,
           title: 'Tito',
-          theme: ThemeData(
-            primarySwatch: Colors.deepPurple,
+          theme: ThemeData( 
+            primaryColor: ColorSystem.purple,
           ),
         ),
       ),
