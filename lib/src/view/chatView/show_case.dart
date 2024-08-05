a10196a16bbc810f29dd1ee000ea4113baa48e5f
kimjunha <tto9011@naver.com>
2024-08-06 04:27:37 +0900

    [feat] 쇼케이스 크기 맞추기

--- lib/src/view/chatView/show_case.dart
+++ lib/src/view/chatView/show_case.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:showcaseview/showcaseview.dart';
+import 'package:tito_app/core/constants/style.dart';
+import 'package:tito_app/src/view/chatView/chat_view_details.dart';
 
 class ShowCase extends StatelessWidget {
   @override
@@ -49,167 +51,251 @@ class _ShowCaseScreenState extends State<ShowCaseScreen> {
             child: Icon(Icons.more_vert, color: Colors.black),
           ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Container(
+            color: Colors.grey,
+            height: 1.0,
+          ),
+        ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.all(16.0),
-              children: [
-                Showcase(
-                  key: _keyTimer,
-                  description: '포키님의 반론 타임이에요!',
-                  tooltipBackgroundColor: Colors.black,
-                  textColor: Colors.white,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          '🕑 7:20 남았어요!',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          color: ColorSystem.grey3,
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(bottom: 5),
+              color: ColorSystem.white,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/detailChatIcon.svg',
                           ),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          '🛎 발언 제안 시간이 카운팅 돼요!',
-                          style: TextStyle(
-                            color: Colors.purple,
-                            fontWeight: FontWeight.bold,
+                          const SizedBox(width: 8),
+                          Text(
+                            '포키님의 반론타임이에요!',
+                            style: FontSystem.KR16R,
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Showcase(
-                  key: _keySuggestion,
-                  description: 'LLM의 작성 코칭!',
-                  tooltipBackgroundColor: Colors.purple,
-                  textColor: Colors.white,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.purple.shade100,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Text(
-                      '지금 작성하신 주장은 주제에서 벗어난 것 같아요. 파미의 역설에 대한 반박글을 한 문장으로 작성하는 것을 추천해요!',
-                      style: TextStyle(
-                        color: Colors.purple,
-                        fontWeight: FontWeight.bold,
+                        ],
                       ),
                     ),
                   ),
-                ),
-                Showcase(
-                  key: _keyMessage,
-                  description: '상대 의견 작성 타임이에요!',
-                  tooltipBackgroundColor: Colors.grey,
-                  textColor: Colors.white,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.grey),
+                  const SizedBox(height: 3),
+                  Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Showcase(
+                            key: _keyTimer,
+                            description: '⏳발언 제안 시간이 카운팅 돼요!',
+                            tooltipBackgroundColor: ColorSystem.purple,
+                            targetPadding: EdgeInsets.all(10),
+                            targetBorderRadius: BorderRadius.circular(12),
+                            tooltipPadding: EdgeInsets.all(10),
+                            tooltipBorderRadius: BorderRadius.circular(12),
+                            textColor: Colors.white,
+                            child: Text(
+                              '⏳ 7:20 토론 시작 전',
+                              style: FontSystem.KR16B
+                                  .copyWith(color: ColorSystem.purple),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '외계인 있다? 없다?👽',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          '이 넓은 우주에 우리가 유일한 생명체일 리가 없다고 생각해요. 은하만 해도 수백 억 개가 있는데 그중에 지구 같은 조건을 가진 행성이 없을까요?',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          '만약에 외계인이 정말 있다면 왜 우리랑 접촉을 안 하겠어요? 파미의 역설이라는 게 있잖아요 그동안 아무런 접촉이 없었다는 건 애초에 없다는 증거 아닐까요?',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        SizedBox(height: 5),
-                        Row(
+                  ),
+                ],
+              ),
+            ),
+            Showcase(
+              key: _keyMessage,
+              description: '상대 의견 작성 타임이에요!',
+              tooltipBackgroundColor: Colors.grey,
+              textColor: Colors.white,
+              child: Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: ColorSystem.grey3,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 4.0, horizontal: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: [
-                            CircleAvatar(
-                              child: Icon(Icons.person),
+                            const SizedBox(width: 8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Container(
+                                  constraints:
+                                      const BoxConstraints(maxWidth: 250),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 8.0, horizontal: 16.0),
+                                  decoration: BoxDecoration(
+                                    color: ColorSystem.white,
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                  child: Text(
+                                      '이 넓은 우주에 우리가 유일한 생명체일 리가 없다고 생각해요. 은하만 해도 수백억 개가 있는데 그중에 지구 같은 조건을 가진 행성이 없을까요?'),
+                                ),
+                                const SizedBox(height: 5),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16.0),
+                                  child: Text(
+                                    '10:00pm',
+                                    style: const TextStyle(
+                                        fontSize: 10, color: Colors.black54),
+                                  ),
+                                ),
+                              ],
                             ),
-                            SizedBox(width: 10),
-                            Text(
-                              '타이밍 벨',
-                              style: TextStyle(
-                                color: Colors.purple,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                            const CircleAvatar(child: Icon(Icons.person)),
                           ],
                         ),
-                        SizedBox(height: 5),
-                        Text(
-                          '상대 의견 작성 타임이에요!',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const CircleAvatar(child: Icon(Icons.person)),
+                          const SizedBox(width: 8),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                constraints:
+                                    const BoxConstraints(maxWidth: 250),
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 16.0),
+                                decoration: BoxDecoration(
+                                  color: ColorSystem.white,
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: Text(
+                                    '만약에 외계인이 정말 있다면 왜 우리랑 접촉을 안 하겠어요? 파미의 역설이라는게 있잖아요 그동안 아무런 접촉이 없었다는 건 애초에 없다는 증거 아닌가요?'),
+                              ),
+                              const SizedBox(height: 5),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16.0),
+                                child: Text(
+                                  '10:00pm',
+                                  style: FontSystem.KR12R
+                                      .copyWith(color: ColorSystem.grey),
+                                ),
+                              ),
+                            ],
                           ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: ColorSystem.grey3,
+              ),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: ColorSystem.black,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 4),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icons/timingBell.svg',
+                          width: 20, // 아이콘 크기 조정
+                          height: 20,
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          '타이밍 벨',
+                          style: FontSystem.KR16B
+                              .copyWith(color: ColorSystem.yellow),
                         ),
                       ],
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: SvgPicture.asset('assets/icons/plus.svg'),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  color: ColorSystem.white,
                 ),
-                Expanded(
-                  child: TextField(
-                    autocorrect: false,
-                    decoration: InputDecoration(
-                      hintText: '상대 의견 작성 타임이에요!',
-                      fillColor: Colors.grey[200],
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                        borderSide: BorderSide.none,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset('assets/icons/plus.svg'),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                          hintText: '상대 의견 작성 타임이에요!',
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                        onSubmitted: (value) {},
                       ),
                     ),
-                    onSubmitted: (value) {},
-                  ),
+                    const SizedBox(width: 8),
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset('assets/icons/sendArrow.svg'),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 8),
-                IconButton(
-                  onPressed: () {},
-                  icon: SvgPicture.asset('assets/icons/sendArrow.svg'),
-                ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
