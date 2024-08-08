93da8eaa48efc0320e1d47dcdd28a61f249fca5a
kimjunha <tto9011@naver.com>
2024-08-08 17:58:43 +0900

    [Feat] 차단 post구현

--- lib/core/api/api_service.g.dart
+++ lib/core/api/api_service.g.dart
@@ -206,6 +206,33 @@ class _ApiService implements ApiService {
   }
 
   @override
+  Future<UserProfile> getUserProfile(int debateId) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<UserProfile>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'users/${debateId}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final _value = UserProfile.fromJson(_result.data!);
+    return _value;
+  }
+
+  @override
   Future<List<Debate>> getDebateList(String state) async {
     final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{r'state': state};
---
+++ lib/src/data/models/user_profile.dart
@@ -0,0 +1,51 @@
+class UserProfile {
+  final int id;
+  final String nickname;
+  final String email;
+  final String role;
+  final String? profilePicture;
+  final String createdAt;
+  final String updatedAt;
+  final bool tutorialCompleted;
+  final bool quit;
+
+  UserProfile({
+    required this.id,
+    required this.nickname,
+    required this.email,
+    required this.role,
+    this.profilePicture,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.tutorialCompleted,
+    required this.quit,
+  });
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) {
+    return UserProfile(
+      id: json['data']['id'] ?? 0,
+      nickname: json['data']['nickname'] ?? '',
+      email: json['data']['email'] ?? '',
+      role: json['data']['role'] ?? '',
+      profilePicture: json['data']['profilePicture'],
+      createdAt: json['data']['createdAt'] ?? '',
+      updatedAt: json['data']['updatedAt'] ?? '',
+      tutorialCompleted: json['data']['tutorialCompleted'] ?? false,
+      quit: json['data']['quit'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nickname': nickname,
+      'email': email,
+      'role': role,
+      'profilePicture': profilePicture,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+      'tutorialCompleted': tutorialCompleted,
+      'quit': quit,
+    };
+  }
+}
