23937287a97ce19ed29b342b53b1b4fba1aec293
Poxios <poxios0310@gmail.com>
2024-08-27 21:14:51 +0900

    [fix] fixed api address

--- lib/core/api/api_service.dart
+++ lib/core/api/api_service.dart
@@ -21,7 +21,7 @@ import 'package:tito_app/src/data/models/debate_hotfighter.dart';
 import 'package:tito_app/src/data/models/debate_benner.dart';
 part 'api_service.g.dart';
 
-@RestApi(baseUrl: "http://192.168.0.2:8080/")
+@RestApi(baseUrl: "https://dev.tito.lat/")
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
