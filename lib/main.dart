1dc44ff6b41d6be9289f46d3e0bf132d60068ee9
kimjunha1231 <tto9011@naver.com>
2024-10-11 17:29:20 +0900

    [feat]  back버튼 추가

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:tito_app/firebase_options.dart';
-
+import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'core/routes/routes.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tito_app/core/constants/style.dart';
 
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'dart:async';
-import 'package:flutter/services.dart'; // 가로방향 막히기
+import 'package:flutter/services.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   RemoteNotification? notification = message.notification;
@@ -151,19 +152,58 @@ Future main() async {
   });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DateTime? currentBackPressTime;
+
+  @override
+  void initState() {
+    super.initState();
+
+    BackButtonInterceptor.add(_interceptor);
+  }
+
+  @override
+  void dispose() {
+    BackButtonInterceptor.remove(_interceptor);
+    super.dispose();
+  }
+
+  bool _interceptor(bool stopDefaultButtonEvent, RouteInfo info) {
+    DateTime now = DateTime.now();
+
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime!) > const Duration(seconds: 2)) {
+      currentBackPressTime = now;
+
+      Fluttertoast.showToast(
+        msg: "뒤로 버튼을 한 번 더 누르시면 종료됩니다.",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+      );
+      return true; // 기본 동작을 막음
+    }
+    return false; // 앱 종료 허용
+  }
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(390, 844), // 피그마에 디자인 된 앱 프레임 사이즈로 맞춰서 적용한 것
-      minTextAdapt: true, // 텍스트 크기를 자동으로 조정하여 화면에 맞추는 기능을 활성화
-      splitScreenMode: true, // 분할 화면 모드를 활성화
+      designSize: const Size(390, 844), // 피그마에 맞춘 사이즈
+      minTextAdapt: true, // 텍스트 크기 자동 조정
+      splitScreenMode: true, // 분할 화면 모드 활성화
       builder: (context, child) => ProviderScope(
         child: MaterialApp.router(
           debugShowCheckedModeBanner: false,
-          routerConfig: router,
+          routerConfig: router, // GoRouter 설정
           title: 'Tito',
           theme: ThemeData(
             scaffoldBackgroundColor: ColorSystem.white,
